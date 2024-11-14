868eaa4da2e8bc4f484876096c8bfd9cc7966b52
duckly216 <jorgep020905@gmail.com>
2024-11-14 09:07:37 -0500

    Created draw_grid method, started to work on mouseclick feature

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,22 @@
 import pygame
-
+import random
+def draw_grid(screen):
+    #horizontal lines
+    for i in range(1,16):
+        pygame.draw.line(
+            screen,
+            "brown",
+            (0,32*i),
+            (640,32*i)
+        )
+    #vertical lines
+    for i in range(1,20):
+        pygame.draw.line(
+            screen,
+            "brown",
+            (32*i,0),
+            (32*i,512)
+        )
 
 def main():
     try:
@@ -14,7 +31,17 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN and running:
+                    x, y = event.pos
+                    row = y // 32
+                    col = x // 32
+                    #print("column: ", col,'row: ', row)
+
             screen.fill("light green")
+
+            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
+            draw_grid(screen)
+
             pygame.display.flip()
             clock.tick(60)
     finally:
